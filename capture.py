e2c34953793e898a0a7f108dc67688bb1ebfb0ba
cduang <42569425+cduang@users.noreply.github.com>
2024-12-27 09:43:35 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -21,21 +21,21 @@ data = {
 headers = {
     "accept": "application/json, text/plain, */*",
     "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5",
-    "baggage": "sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=e696dd4e371145ccb4cba04a7f6f7ad2",
+    "baggage": "sentry-environment=production,sentry-release=dev-1727596539903,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=d3cc3a94f5244647b8064ecd77eb8ba6",
     "cache-control": "no-cache",
     "content-type": "application/json;charset=UTF-8",
     "dnt": "1",
     "origin": "https://weread.qq.com",
     "pragma": "no-cache",
     "priority": "u=1, i",
-    "referer": "https://weread.qq.com/web/reader/ce032b305a9bc1ce0b0dd2ake4d32d5015e4da3b7fbb1fa",
+    "referer": "https://weread.qq.com/web/reader/ce032b305a9bc1ce0b0dd2akd2d32c50249d2ddea18fb39",
     "sec-ch-ua": "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
-    "sentry-trace": "e696dd4e371145ccb4cba04a7f6f7ad2-a0bbd08d3d1c54f9",
+    "sentry-trace": "d3cc3a94f5244647b8064ecd77eb8ba6-93b39e13fa4e5fd6",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
 }
 
@@ -53,13 +53,13 @@ cookies = {
     "fqm_pvqid": "50bb40ea-985c-4d11-9cea-7dfefe6ea1ca",
     "_clck": "15sxecs|1|fl1|0",
     "qq_domain_video_guid_verify": "004329d456c0ef18",
-    "wr_vid": "392502384",
-    "wr_localvid": "e84321b0817651c70e8437b",
-    "wr_name": "n",
+    "wr_vid": "346607432",
+    "wr_localvid": "6a8327b0814a8cf486a8884",
+    "wr_name": "%E6%9C%AC%20%E6%97%A0%20%E9%81%93",
     "wr_gender": "1",
-    "wr_rt": "web%4045N4d2slNlMIARZ~Lq2_AL",
-    "wr_avatar": "https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FQ0j4TwGTfTKI041tOO0X5sWmQs8tX6vRKUhLOlcCwZN6knvj4Xanu17CUDgrLKyqp55H1fMxR3tUkMCiap04qzg%2F132",
+    "wr_rt": "web%40dz_AYa7CIYk07_ucDIb_AL",
+    "wr_avatar": "https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FMCpyjIyiaHicBXjh38REzNMA1xXiaeoWJ321CicmRcyMzeSibgDp1z6XC1FVr4szNr4PUsfIqEPRNXa4l9h2NGQsZDg%2F132",
     "wr_fp": "1659424119",
     "wr_pf": "NaN",
-    "wr_skey": "TFYuPHFm"
+    "wr_skey": "ivnZkd2_"
 }
